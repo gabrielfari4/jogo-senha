dd8c7a29373d4cf774d2e183ddd228650e04fb98
gabrielfari4 <gabrielfaria88@gmail.com>
2025-05-16 11:42:58 -0300

    :lipstick: feat: estilização Legenda

--- src/components/Legenda/index.jsx
+++ src/components/Legenda/index.jsx
@@ -1,16 +1,27 @@
+import { useContext } from 'react';
 import styles from './Legenda.module.css';
+import { SenhaContext } from '../../context/SenhaContext';
 
 const Legenda = () => {
+    const {password} = useContext(SenhaContext);
+
+
     return (
         <section className={styles.container}>
-            <div className={styles.quadrado}>
-                <p className={styles.texto}>letra/número errada(o)</p>
+            <div>
+            <span className={styles.quadrado}>
+            </span>
+                <p className={styles.texto}>{isNaN(password) ? 'letra errada' : 'número errado'}</p>
             </div>
-            <div className={styles.quadrado}>
-                <p className={styles.texto}>letra/número certa(o), posição errada</p>
+            <div>
+                <span className={styles.quadrado}>
+                </span>
+                    <p className={styles.texto}>{isNaN(password) ? 'letra certa, posição errada' : 'número certo, posição errada'}</p>
             </div>
-            <div className={styles.quadrado}>
-                <p className={styles.texto}>letra/número certa(o), posição certa</p>
+            <div>
+                <span className={styles.quadrado}>
+                </span>
+                    <p className={styles.texto}>{isNaN(password) ? 'letra certa, posição certa' : 'número certo, posição certa'}</p>
             </div>
         </section>
     )
--- src/components/Senha/index.jsx
+++ src/components/Senha/index.jsx
@@ -6,6 +6,7 @@ import Adivinhar from "../Adivinhar";
 import Tentativas from "../Tentativas";
 import styles from './Senha.module.css';
 import Gif from "../Gif";
+import Legenda from "../Legenda";
 
 const Senha = () => {
     const { password, randomWord, randomNumber, win, count, guesses, guessesCount } =
@@ -22,21 +23,22 @@ const Senha = () => {
         <>
             <h1>Senha</h1>
             {guesses.length >= 1 || (
-                <div className={styles.modo}>
-                    <Modo 
-                        value={randomWord} 
-                        checked={password === randomWord}>
-                        Palavra
-                    </Modo>
-                    <Modo
-                        value={randomNumber}
-                        checked={password === randomNumber}
-                        variante="variante"
-                    >
-                        Número
-                    </Modo>
-                </div>
-            )}
+                    <div className={styles.modo}>
+                        <Modo 
+                            value={randomWord} 
+                            checked={password === randomWord}>
+                            Palavra
+                        </Modo>
+                        <Modo
+                            value={randomNumber}
+                            checked={password === randomNumber}
+                            variante="variante"
+                        >
+                            Número
+                        </Modo>
+                    </div>
+                )
+            }
             {password === randomWord ? (
                 <p className={styles.texto}>Adivinhe a senha de 5 letras!</p>
             ) : (
@@ -61,6 +63,7 @@ const Senha = () => {
                 ? <p className={styles.texto}>Parabéns! Resposta correta!</p>
                 : <p className={styles.texto}>Você perdeu...<br/>A senha é {password.toUpperCase()}</p>
             }
+            {guesses.length >= 1 && <Legenda />}
             <div>
                 <ul>
                     {guesses.map((element, index) => {
